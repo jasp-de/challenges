aa2db151a7e5439d5fb8e23038711c98b5071b21
jasp_de <d.j.deppermann@gmail.com>
2024-09-16 16:08:27 +0200

    solv challenge

--- js-loops_stars/js/index.js
+++ js-loops_stars/js/index.js
@@ -1,14 +1,29 @@
 console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
+let filledStars = 3;
 
-function renderStars() {
+function renderStars(filledStars) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
+  for (let counter = 1; counter <= 5; counter++) {
+    // create stars and append to container
+    const starImage = document.createElement("img");
+    if (counter <= filledStars) {
+      starImage.src = "./assets/star-filled.svg";
+    } else {
+      starImage.src = "./assets/star-empty.svg";
+    }
+    starImage.addEventListener("click", () => {
+      renderStars(counter);
+    });
+
+    starContainer.append(starImage); // placing the created button into the article
+  }
 
   //--^-- your code here --^--
 }
 
-renderStars();
+renderStars(3);
