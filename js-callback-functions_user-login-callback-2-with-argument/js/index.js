e9eae9f6f53f6ac358d830807648a1f8d100a422
jasp_de <d.j.deppermann@gmail.com>
2024-09-17 11:18:11 +0200

    solve challenge

--- js-callback-functions_user-login-callback-2-with-argument/js/index.js
+++ js-callback-functions_user-login-callback-2-with-argument/js/index.js
@@ -5,3 +5,20 @@ function handleUserLogin(onSuccess) {
 }
 
 // The exercise starts here!
+let userName = "Jasper";
+
+function showWelcomeMessage(userName) {
+  console.log("Welcome " + userName + ", You are logged in now.");
+}
+
+console.log("showWelcomeMessage(userName);");
+showWelcomeMessage(userName);
+
+console.log("handleUserLogin(showWelcomeMessage);");
+handleUserLogin(showWelcomeMessage);
+
+console.log("handleUserLogin with anon");
+
+handleUserLogin(() => {
+  console.log("Welcome " + userName + ", You are logged in now.");
+});
