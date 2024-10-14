11bd9775f06d06a3e6e98a3cb544315e08886a9a
jasp_de <d.j.deppermann@gmail.com>
2024-10-14 11:01:20 +0200

    solved challenge

--- lotr-app/pages/volumes/index.js
+++ lotr-app/pages/volumes/index.js
@@ -1,28 +1,23 @@
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
 import { introduction, volumes } from "../../lib/data.js";
+import Volumes from "../../components/Volumes.js";
+import VolumeButton from "../../components/VolumeButton.js";
+import { useRouter } from "next/router";
 
-export default function Volumes() {
+export default function LOTR() {
+  const router = useRouter();
+
+  function handleRandom() {
+    const randomVolume = volumes[Math.floor(Math.random() * volumes.length)];
+    router.push(`/volumes/${randomVolume.slug}`);
+  }
+
   return (
     <section>
       <h1>Lord of the Rings - All Volumes</h1>
       <p>{introduction}</p>
-      <ul>
-        {volumes.map((volume) => (
-          <li key={volume.slug}>
-            <Image
-              src={volume.cover}
-              alt={volume.title}
-              width={140}
-              height={230}
-            />
-            <Link href={`/volumes/${volume.slug}`}>
-              <h2>{volume.title}</h2>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <Volumes volumes={volumes} />
+      <VolumeButton onRandom={handleRandom} />
     </section>
   );
 }
